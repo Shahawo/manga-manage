e5f60dd47bee2e77d592fdcf7600a08c428f1104
Shahawo <dactam172@gmail.com>
2026-05-10 03:01:09 +0700

    Nâng cấp lên Vite & Modularization

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Base URL phải trùng với tên repository trên GitHub
-  base: '/manga-manage/',
+  // Khi sử dụng CNAME (Custom Domain), base URL phải là gốc '/'
+  base: '/',
 });
