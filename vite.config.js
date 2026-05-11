e5aade3351cba10a092b399c9d15137bb542dee4
Shahawo <dactam172@gmail.com>
2026-05-11 19:04:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,20 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Khi sử dụng CNAME (Custom Domain), base URL phải là gốc '/'
   base: '/',
+  build: {
+    target: 'esnext',
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('/src/mixins/')) {
+            return 'mixins';
+          }
+        }
+      }
+    }
+  }
 });
