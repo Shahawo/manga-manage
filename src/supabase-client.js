a46f5daecbfe400550a2aa8921f256df7ba286a0
Shahawo <dactam172@gmail.com>
2026-05-11 19:25:03 +0700

    Update supabase-client.js

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -6,8 +6,12 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://uviyjgbgqqsozmkfdvbl.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2aXlqZ2JncXFzb3pta2ZkdmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2Nzc5NTcsImV4cCI6MjA5MjI1Mzk1N30.eBmEf5t4DQA5hsgMeKuvpO9mMpBTUbgqsJ-EdSWeJg8';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+    console.error('Missing Supabase credentials! Please check your .env file.');
+}
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
