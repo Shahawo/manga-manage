9aae48f9576b3cb93f3dd7eceff0f673e2b3a5a1
Shahawo <dactam172@gmail.com>
2026-06-22 00:21:56 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  // Khi sử dụng CNAME (Custom Domain), base URL phải là gốc '/'
-  base: '/',
-  build: {
-    target: 'esnext',
-    cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-          if (id.includes('/src/mixins/')) {
-            return 'mixins';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // Khi sử dụng CNAME (Custom Domain), base URL phải là gốc '/'
+    base: '/',
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'https://manga-cloudflare-worker.dactam172.workers.dev',
+          changeOrigin: true,
+        }
+      }
+    },
+    build: {
+      target: 'esnext',
+      cssMinify: true,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+            if (id.includes('/src/mixins/')) {
+              return 'mixins';
+            }
           }
         }
       }
     }
-  }
+  };
 });
